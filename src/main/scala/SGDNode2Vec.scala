2d76157624b2f83cd6f41376f4d6b5027d45c9eb
Youssef Youssry <youssef.yossry.2014@gmail.com>
2019-04-05 02:03:32 +0300

    Adding SGD for Node2Vec.

--- src/main/scala/SGDNode2Vec.scala
+++ src/main/scala/SGDNode2Vec.scala
@@ -1,14 +1,57 @@
 
-import breeze.linalg.DenseMatrix
+import breeze.linalg.{Axis, DenseMatrix, DenseVector, sum}
+import breeze.numerics.sigmoid
+import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 
+import scala.util.Random
 
-class SGDNode2Vec(embSize: Int, nodes: Int) {
-  val embeddingIn = new DenseMatrix[Double](rows = nodes, cols = embSize)
-  val embeddingOut = new DenseMatrix[Double](rows = nodes, cols = embSize)
 
-  def fit(batches: Array[RDD[(Int, Int)]], learningRate: Double, epochs: Int, negativeSamples: Int)
+class SGDNode2Vec(embSize: Int, nodes: Int, sc: SparkContext) {
+  val rand = new Random()
+  val embeddingIn = new DenseMatrix(rows = embSize, cols = nodes,
+    Array.fill(nodes * embSize)(rand.nextDouble()))
+  val embeddingOut = new DenseMatrix(rows = embSize, cols = nodes,
+    Array.fill(nodes * embSize)(rand.nextDouble()))
+
+  def fit(batches: Array[RDD[(Int, Int)]], learningRate: Double,
+          epochs: Int, negativeSamples: Int, batchSize: Double)
   : (DenseMatrix[Double], DenseMatrix[Double]) = {
+
+    for (epoch <- 1 to epochs) {
+      val embInBroadcast = sc.broadcast(embeddingIn)
+      val embOutBroadcast = sc.broadcast(embeddingOut)
+      for (batch <- batches) {
+        val gradients = batch.map(edge => estimateEdgeGradients(edge._1, edge._2, negativeSamples,
+          embInBroadcast.value, embOutBroadcast.value))
+        val inGrads = gradients.map(x => x._1).reduceByKey(_ + _)
+        val outGrads = gradients.map(x => x._2).reduceByKey(_ + _)
+        inGrads.foreach(x => {
+          embeddingIn(::, x._1) += -learningRate * x._2 / batchSize
+        })
+        outGrads.foreach(x => {
+          embeddingOut(::, x._1) += -learningRate * x._2 / batchSize
+        })
+      }
+    }
     (embeddingIn, embeddingOut)
+  }
+
+  def estimateEdgeGradients(source: Int, destination: Int, negatives: Int,
+                            embInBroadcast: DenseMatrix[Double], embOutBroadcast: DenseMatrix[Double]):
+  ((Int, DenseVector[Double]), (Int, DenseVector[Double])) = {
+
+    val in = embInBroadcast(::, source)
+    val out = embOutBroadcast(::, destination)
+    val negSamples = embOutBroadcast(::, IndexedSeq.fill(negatives)(rand.nextInt(nodes))).toDenseMatrix
+
+    val sigmoids = sigmoid.apply(negSamples.t * in)
+    val temp = negSamples.mapPairs({
+      case ((_, col), value) => value * sigmoids(col)
+    })
+    val inGrad = out * -sigmoid.apply(-in.dot(out)) + sum(temp, Axis._1)
+    val outGrad = in * -sigmoid.apply(-in.dot(out))
+
+    ((source, inGrad), (destination, outGrad))
   }
 }
